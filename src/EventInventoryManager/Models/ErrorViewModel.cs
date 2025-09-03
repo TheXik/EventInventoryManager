2d6d079ab78638ec0dff91687a69ec61ca20e51e
Lukas <lukas.hellesch@gmail.com>
2025-09-03 14:12:58 +0200

    add initial project

---
+++ src/EventInventoryManager/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EventInventoryManager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
