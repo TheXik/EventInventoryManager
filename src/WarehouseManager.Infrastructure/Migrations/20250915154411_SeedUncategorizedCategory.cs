7c9d22fcd848da2d47d89b939533274b1d0bf4b7
hellescl <lukas.hellesch@gmail.com>
2025-09-15 19:26:25 +0200

    migrations of the database

---
+++ src/WarehouseManager.Infrastructure/Migrations/20250915154411_SeedUncategorizedCategory.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WarehouseManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedUncategorizedCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 1, null, "Uncategorized" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InventoryItems_CategoryId",
+                table: "InventoryItems",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InventoryItems_Categories_CategoryId",
+                table: "InventoryItems",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InventoryItems_Categories_CategoryId",
+                table: "InventoryItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_InventoryItems_CategoryId",
+                table: "InventoryItems");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
