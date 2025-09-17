ffc5a6f2ec5b7f1e917ea36ceb4bb358fe0f1fe3
hellescl <lukas.hellesch@gmail.com>
2025-09-17 19:54:24 +0200

    addnewfields to event migration

---
+++ src/WarehouseManager.Infrastructure/Migrations/20250917072313_addNewFieldsToEvent.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WarehouseManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addNewFieldsToEvent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Events",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "Events",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Events",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RecurrenceRule",
+                table: "Events",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "RecurrenceRule",
+                table: "Events");
+        }
+    }
+}
