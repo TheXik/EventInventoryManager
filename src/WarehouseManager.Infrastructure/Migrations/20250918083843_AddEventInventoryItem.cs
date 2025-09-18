0dde9de672edb29527e0b231659eb505d19fef16
hellescl <lukas.hellesch@gmail.com>
2025-09-18 14:00:59 +0200

    new migrations

---
+++ src/WarehouseManager.Infrastructure/Migrations/20250918083843_AddEventInventoryItem.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WarehouseManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEventInventoryItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InventoryItems_Events_EventId",
+                table: "InventoryItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_InventoryItems_EventId",
+                table: "InventoryItems");
+
+            migrationBuilder.DropColumn(
+                name: "EventId",
+                table: "InventoryItems");
+
+            migrationBuilder.CreateTable(
+                name: "EventInventoryItems",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(type: "INTEGER", nullable: false),
+                    InventoryItemId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Id = table.Column<int>(type: "INTEGER", nullable: false),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventInventoryItems", x => new { x.EventId, x.InventoryItemId });
+                    table.ForeignKey(
+                        name: "FK_EventInventoryItems_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EventInventoryItems_InventoryItems_InventoryItemId",
+                        column: x => x.InventoryItemId,
+                        principalTable: "InventoryItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventInventoryItems_InventoryItemId",
+                table: "EventInventoryItems",
+                column: "InventoryItemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventInventoryItems");
+
+            migrationBuilder.AddColumn<int>(
+                name: "EventId",
+                table: "InventoryItems",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InventoryItems_EventId",
+                table: "InventoryItems",
+                column: "EventId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InventoryItems_Events_EventId",
+                table: "InventoryItems",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ src/WarehouseManager.Infrastructure/Migrations/20250918091111_AddTotalAdnAvailibleQuantityToInventoryItem.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WarehouseManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTotalAdnAvailibleQuantityToInventoryItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Quantity",
+                table: "InventoryItems",
+                newName: "TotalQuantity");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AvailableQuantity",
+                table: "InventoryItems",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvailableQuantity",
+                table: "InventoryItems");
+
+            migrationBuilder.RenameColumn(
+                name: "TotalQuantity",
+                table: "InventoryItems",
+                newName: "Quantity");
+        }
+    }
+}
